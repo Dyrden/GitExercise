fa2093a15ae1c5d870781f9415c6e2dc6ea7cb97
Mark <mark771e@stud.kea.dk>
2022-02-25 13:40:59 +0100

    added greet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("it works!");
+        Main main = new Main();
+        main.greet();
 
     }
 
-
-
+    public void greet() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("indtast navn : ");
+        String navn = sc.nextLine();
+        System.out.println("hej " + navn);
+    }
 }
